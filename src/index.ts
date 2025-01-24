4ee87aebc785c0e9fe6b1e61467e55b1b289d7b4
Bram del Canho <bramdelcanho@gmail.com>
2025-01-24 17:13:06 +0100

    Remove search parameter from id

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { transformWithEsbuild } from 'vite'
 import flowRemoveTypes from 'flow-remove-types'
-import fs from 'fs/promises'
+import fs from 'node:fs/promises'
 
 import type { Plugin as VitePlugin } from 'vite'
 import type { Plugin as ESBuildPlugin } from 'esbuild'
@@ -9,96 +9,98 @@ import type { Plugin as ESBuildPlugin } from 'esbuild'
 const development = process.env.NODE_ENV === 'development'
 
 const extensions = [
-    '.web.mjs',
-    '.mjs',
-    '.web.js',
-    '.js',
+	'.web.mjs',
+	'.mjs',
+	'.web.js',
+	'.js',
 
-    '.web.mts',
-    '.mts',
-    '.web.ts',
-    '.ts',
+	'.web.mts',
+	'.mts',
+	'.web.ts',
+	'.ts',
 
-    '.web.jsx',
-    '.jsx',
+	'.web.jsx',
+	'.jsx',
 
-    '.web.tsx',
-    '.tsx',
+	'.web.tsx',
+	'.tsx',
 
-    '.json',
+	'.json',
 ]
 
 const loader = {
-    '.js': 'jsx',
+	'.js': 'jsx',
 } as const
 
 const filter = /\.(js|flow)$/
 
 const esbuildPlugin = (): ESBuildPlugin => ({
-    name: 'react-native-web',
-    setup: (build) => {
-        build.onLoad({ filter }, async ({ path }) => {
-            const src = await fs.readFile(path, 'utf-8')
-            return {
-                contents: flowRemoveTypes(src).toString(),
-                loader: loader['.js'],
-            }
-        })
-    },
+	name: 'react-native-web',
+	setup: (build) => {
+		build.onLoad({ filter }, async ({ path }) => {
+			const src = await fs.readFile(path, 'utf-8')
+			return {
+				contents: flowRemoveTypes(src).toString(),
+				loader: loader['.js'],
+			}
+		})
+	},
 })
 
 const reactNativeWeb = (/*options: ViteReactNativeWebOptions = {}*/): VitePlugin => ({
-    enforce: 'pre',
-    name: 'react-native-web',
+	enforce: 'pre',
+	name: 'react-native-web',
 
-    config: () => ({
-        define: {
-            global: 'window',
-            __DEV__: JSON.stringify(development),
-            'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
-        },
-        resolve: {
-            extensions,
-            alias: [{ find: 'react-native', replacement: 'react-native-web' }],
-        },
-        optimizeDeps: {
-            esbuildOptions: {
-                plugins: [esbuildPlugin()],
-                resolveExtensions: extensions,
-            },
-        },
-    }),
+	config: () => ({
+		define: {
+			global: 'window',
+			__DEV__: JSON.stringify(development),
+			'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+		},
+		resolve: {
+			extensions,
+			alias: [{ find: 'react-native', replacement: 'react-native-web' }],
+		},
+		optimizeDeps: {
+			esbuildOptions: {
+				plugins: [esbuildPlugin()],
+				resolveExtensions: extensions,
+			},
+		},
+	}),
 
-    async transform(code, id) {
-        if (!filter.test(id)) {
-            return
-        }
+	async transform(code, id) {
+		id = id.split('?')[0]
 
-        let includeSourceMaps = true
+		if (!filter.test(id)) {
+			return
+		}
 
-        // Do not include source maps for files that are using 'use client' pragma since these break the esbuild mappings (https://github.com/vitejs/vite/issues/15012)
-        if (code.includes('\'use client\'') || code.includes('"use client"')) {
-            includeSourceMaps = false
-        }
+		let includeSourceMaps = true
 
-        if (code.includes('@flow')) {
-            code = flowRemoveTypes(code).toString()
-        }
+		// Do not include source maps for files that are using 'use client' pragma since these break the esbuild mappings (https://github.com/vitejs/vite/issues/15012)
+		if (code.includes("'use client'") || code.includes('"use client"')) {
+			includeSourceMaps = false
+		}
 
-        const result = await transformWithEsbuild(code, id, {
-            loader: loader['.js'],
-            tsconfigRaw: {
-                compilerOptions: {
-                    jsx: 'react-jsx',
-                },
-            },
-        })
+		if (code.includes('@flow')) {
+			code = flowRemoveTypes(code).toString()
+		}
 
-        return {
-            code: result.code,
-            map: includeSourceMaps ? result.map : null,
-        }
-    },
+		const result = await transformWithEsbuild(code, id, {
+			loader: loader['.js'],
+			tsconfigRaw: {
+				compilerOptions: {
+					jsx: 'react-jsx',
+				},
+			},
+		})
+
+		return {
+			code: result.code,
+			map: includeSourceMaps ? result.map : null,
+		}
+	},
 })
 
 export default reactNativeWeb
