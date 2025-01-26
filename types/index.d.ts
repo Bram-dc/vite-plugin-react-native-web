2e8a979348a163c333fa3d7c24a51adf16a8b970
Bram-dc <bramdelcanho@gmail.com>
2025-01-26 14:44:44 +0100

    Disable noParameterAssign rule in biome linter and add ignore comment for complexity linting in type definitions

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,6 @@
 import type { Plugin as VitePlugin } from 'vite'
 
+// biome-ignore lint/complexity/noBannedTypes: Empty type for now
 export type ViteReactNativeWebOptions = {}
 
 export default function reactNativeWeb(options?: ViteReactNativeWebOptions): VitePlugin
