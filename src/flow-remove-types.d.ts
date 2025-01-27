683b3dd2d8a8e2f44efd9f07c86482f0742331d1
Bram-dc <bramdelcanho@gmail.com>
2025-01-27 12:54:45 +0100

    Update flow-remove-types to 2.259.1 and add source map generation support

--- src/flow-remove-types.d.ts
+++ src/flow-remove-types.d.ts
@@ -1,5 +1,11 @@
 declare module 'flow-remove-types' {
 	export default function flowRemoveTypes(src: string): {
 		toString(): string
+		generateMap(): {
+			mappings: string
+			names: string[]
+			sources: string[]
+			version: number
+		}
 	}
 }
