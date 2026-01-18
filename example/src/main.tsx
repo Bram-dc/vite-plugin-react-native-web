fc81f8b60bb4ff7b1f5d94328494d070684cdcbf
Bram del Canho <bramdelcanho@gmail.com>
2026-01-18 17:21:29 +0100

    Setup correct linter

--- example/src/main.tsx
+++ example/src/main.tsx
@@ -3,7 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
-createRoot(document.getElementById('root')!).render(
+const root = document.getElementById('root')
+if (!root) {
+	throw new Error('Root element not found')
+}
+
+createRoot(root).render(
 	<StrictMode>
 		<App />
 	</StrictMode>,
