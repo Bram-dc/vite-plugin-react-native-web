4ee29a00ffba85080ed153b008a6d84ab86322fa
Bram del Canho <bramdelcanho@gmail.com>
2026-03-13 16:47:04 +0100

    refactor: update flow-remove-types plugin to filter by code pattern instead of file extension

--- src/plugins/flow-remove-types-plugin.ts
+++ src/plugins/flow-remove-types-plugin.ts
@@ -1,13 +1,11 @@
 import flowRemoveTypes from 'flow-remove-types'
 import type { RolldownPlugin } from 'rolldown'
 
-const reactNativeFlowJsxPathPattern = /\.(js|flow)$/
-
 export const flowRemoveTypesPlugin = (): RolldownPlugin => ({
 	name: 'flow-remove-types',
 	transform: {
 		filter: {
-			id: reactNativeFlowJsxPathPattern,
+			code: /@flow/,
 		},
 		handler: async (code) => {
 			const transformed = flowRemoveTypes(code)
